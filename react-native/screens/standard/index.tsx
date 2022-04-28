12526337141d680a5bb1ec9aa7cd8c47bf51896a
Gustavo Emanuel Farias Rosa <gustavoemanuelfariasrosa@Gustavos-Mac-mini.local>
2022-04-28 00:37:30 -0300

    fix

--- react-native/screens/standard/index.tsx
+++ react-native/screens/standard/index.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-
-import styled from "styled-components/native"
 import { colors } from '../../colors';
 import { ButtonApp } from '../components/Button';
-import { WrapperScreens } from "../components/wrapperScreens"
 import { TextStandardLogo, WrapperButtonsStandard } from './styled';
-
 import { NativeModules, NativeEventEmitter, Alert } from 'react-native'
-
 const TorchEvents = new NativeEventEmitter(NativeModules.TorchManager)
-
 
 export const StandardScreen = ()=>{
     const[level, setLevel] = React.useState(0)
@@ -18,7 +12,7 @@ export const StandardScreen = ()=>{
         res => {
             setLevel(res.count)
             console.log(res.count)
-            Alert.alert('Battery Level', `${res.count}`)
+            Alert.alert('Battery Level', `Level: ${res.count}`)
         }
     )
     const TurnOnTorch = ()=>{
